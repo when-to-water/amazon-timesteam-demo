f0534b71f37f358c5cb530a586243ac696206174
BeneKenobi <1708125+BeneKenobi@users.noreply.github.com>
2022-05-10 12:37:45 +0000

    style: 🎨 add formater/linters and format accordingly

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
 import boto3
 import time
 
-DB_NAME="demo"
-TBL_NAME="IoT"
+DB_NAME = "demo"
+TBL_NAME = "IoT"
 CURRENT_TIME = str(int(time.time() * 1000))
 
 write_client = boto3.client("timestream-write")
 
-dimension = [ {'Name': 'fleet', 'Value': 'test'}, {'Name': 'fuel_capacity', 'Value': 'test'}, {'Name': 'load_capacity', 'Value': 'test'}, {'Name': 'make', 'Value': 'test'}, {'Name': 'model', 'Value': 'test'}, {'Name': 'truck_id', 'Value': 'test'}]
-record = {'Time': CURRENT_TIME, 'Dimensions': dimension, 'MeasureName': 'test', 'MeasureValue': '11.1','MeasureValueType': 'DOUBLE'}
+dimension = [
+    {"Name": "fleet", "Value": "test"},
+    {"Name": "fuel_capacity", "Value": "test"},
+    {"Name": "load_capacity", "Value": "test"},
+    {"Name": "make", "Value": "test"},
+    {"Name": "model", "Value": "test"},
+    {"Name": "truck_id", "Value": "test"},
+]
+record = {
+    "Time": CURRENT_TIME,
+    "Dimensions": dimension,
+    "MeasureName": "test",
+    "MeasureValue": "11.1",
+    "MeasureValueType": "DOUBLE",
+}
 record = [record]
 
 try:
-    response = write_client.write_records(DatabaseName=DB_NAME,TableName=TBL_NAME,Records=record)
+    response = write_client.write_records(
+        DatabaseName=DB_NAME, TableName=TBL_NAME, Records=record
+    )
     print(response)
-except:
-    print("An exeption occured")
-
+except Exception as e:
+    print(f"An exeption occured: {e}")
